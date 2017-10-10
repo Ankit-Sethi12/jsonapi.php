bd712409eae53876856e13efe791da835c31eda5
Ankit-Sethi12 <32670860+Ankit-Sethi12@users.noreply.github.com>
2017-10-10 13:36:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,4 @@ header('Content-Type: application/json');
 $jsonOutput = json_encode($items);
 echo $jsonOutput;
 
-echo <p> "<a href = 'peopleapi.php'> This link to People Api</a>" </p>
+echo <p> "<a href = 'peopleapi.php'> This link to People Api</a>" </p>;
