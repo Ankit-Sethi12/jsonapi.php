67ee237d7fe27bc4654666e7806b459acc32023a
Ankit-Sethi12 <32670860+Ankit-Sethi12@users.noreply.github.com>
2017-10-10 12:59:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ $items = array();
 for($i = 0; $i < 10; $i++) {
 $items[$i] = "This is message $i";
 }
-header(,Content-Type: application/json,);
+header('Content-Type: application/json');
 $jsonOutput = json_encode($items);
 echo $jsonOutput;
